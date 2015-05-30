f05822ef10087130591fbec5ece8717e703a740a
jff <joaofnfernandes@gmail.com>
2015-05-30 19:17:39 +0100

    Implemented hashtable with open addressing. Added the necessary tests.

---
+++ HashTable/src/OpenAddressing/KeyValuePair.java
@@ -0,0 +1,11 @@
+package OpenAddressing;
+
+public class KeyValuePair<K,V> {
+	public K Key;
+	public V Value;
+	
+	public KeyValuePair(K key, V value) {
+		this.Key = key;
+		this.Value = value;
+	}
+}
