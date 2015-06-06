fdacc8330482f23811907af6653c0bc191955304
jff <joaofnfernandes@gmail.com>
2015-06-06 17:39:38 +0100

    Fixing off-by-one bug in ReverseString

--- chapter-1/src/reverseString/ReverseString.java
+++ chapter-1/src/reverseString/ReverseString.java
@@ -3,7 +3,7 @@ package reverseString;
 public class ReverseString {
 	
 	public static void reverseIterative(char[] s, int length) {
-		for(int i = 0; i < (length - 2) / 2; i++) {
+		for(int i = 0; i < (length - 1) / 2; i++) {
 			swap(s, i, length - 2 - i );		
 		}
 	}
