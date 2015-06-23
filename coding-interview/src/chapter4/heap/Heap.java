9ae1ca895986fe7cceb475fb9c15ee0d689555a1
jff <joaofnfernandes@gmail.com>
2015-06-23 12:39:47 +0100

    heap now uses an arraylist to be dynamic implemented pop()

--- coding-interview/src/chapter4/heap/Heap.java
+++ coding-interview/src/chapter4/heap/Heap.java
@@ -1,28 +1,44 @@
 package chapter4.heap;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+// A max-heap
 public class Heap<T extends Comparable<T>> {
 	private static final int FIRST = 1;
+	private static final int INITIAL_CAPACITY = 10;
 	
-	private T[] arr = null;
+	private ArrayList<T> arr = null;
 	private int last = FIRST;
 	
 	
-	@SuppressWarnings("unchecked")
 	public Heap(T[] values) {
-		this.arr = (T[])Array.newInstance(values[0].getClass(), values.length + 1);
+		arr = new ArrayList<T>(INITIAL_CAPACITY);
+		
+		//add a dummy element at [0] so that we have 1-based indexing
+		arr.add(null);
+		
 		for(T value : values) {
 			insert(value);
 		}
 	}
 	
+	public T max() {
+		if(last == FIRST) {
+			throw new EmptyHeapException();
+		}
+		return arr.get(FIRST);
+	}
+
+	public boolean contains(T value) {
+		return contains(FIRST, value);
+	}
+	
 	public void insert(T value) {
-		arr[last] = value;
+		arr.add(last, value);
 		int curr = last;
 		int parent = last / 2;
 		while(parent >= 1) {
-			if(arr[curr].compareTo(arr[parent]) > 0) {
+			if(arr.get(curr).compareTo(arr.get(parent)) > 0) {
 				swap(arr, curr, parent);
 			} else {
 				break;
@@ -32,28 +48,31 @@ public class Heap<T extends Comparable<T>> {
 		}
 		last++;
 	}
-	
-	public T max() {
-		if(last == FIRST) {
-			throw new EmptyHeapException();
+
+	public T pop() {
+		T value = arr.get(--last);
+		for(int i = FIRST + 1; i < last; i = i*2) {
+			if(arr.get(i / 2).compareTo(arr.get(i)) < 0) {
+				swap(arr, i / 2, i);
+			} else if(arr.get(i / 2).compareTo(arr.get(i + 1)) < 0) {
+				swap(arr, i / 2, i + 1);
+			} else {
+				break;
+			}
 		}
-		return arr[FIRST];
+		return value;
 	}
 
-	public boolean contains(T value) {
-		return contains(FIRST, value);
-	}
-	
-	private void swap(T[] arr, int i, int j) {
-		T aux = arr[i];
-		arr[i] = arr[j];
-		arr[j] = aux;
+	private void swap(ArrayList<T> arr, int i, int j) {
+		T aux = arr.get(i);
+		arr.set(i, arr.get(j));
+		arr.set(j, aux);
 	}
 	
 	private boolean contains(int i, T value) {
 		if(i >= last) {
 			return false;
-		} else if(arr[i].compareTo(value) == 0) {
+		} else if(arr.get(i).compareTo(value) == 0) {
 			return true;
 		} else {
 			return contains(i * 2, value) || contains(i * 2 + 1, value);
--- coding-interview/src/chapter4/heap/TestHeap.java
+++ coding-interview/src/chapter4/heap/TestHeap.java
@@ -13,7 +13,29 @@ public class TestHeap {
 		for(int i = 0; i < values.length; i++) {
 			assertTrue(heap.contains(values[i]));
 		}
+	}
+	
+	@Test
+	public void TestMax() {
+		Integer[] initialValue = new Integer[] {0,1,2};
+		Heap<Integer> heap = new Heap<>(initialValue);
 		
+		assertEquals(initialValue[2], heap.max());
+				
+		for(int i = 3; i < 10; i++) {
+			heap.insert(i);
+			assertEquals(new Integer(i), heap.max());
+		}
+	}
+	
+	@Test
+	public void TestPop() {
+		Integer[] values = new Integer[] {0,1,2,3,4,5};
+		Heap<Integer> heap = new Heap<>(values);
+		
+		for(int i = values.length - 1; i <= 0; i++) {
+			assertEquals(values[i], heap.pop());
+		}
 	}
 
 }
