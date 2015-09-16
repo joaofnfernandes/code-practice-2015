bc791c48619085b125d48f05124b4397b8b88abb
jff <joaofnfernandes@gmail.com>
2015-09-16 12:51:06 -0700

    implemented subset

---
+++ algs-class/src/week2/Subset.java
@@ -0,0 +1,38 @@
+package week2;
+
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+
+public class Subset {
+    // todo: we only need rq of length k
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("usage: java Subset n");
+            return;
+        }
+        int k = Integer.parseInt(args[0]);
+        if (k <= 0) {
+            return;
+        }
+        In in = new In();
+        Out out = new Out();
+        
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        synchronized (rq) {
+            try {
+                rq.wait(1000);
+            } catch (InterruptedException e) {
+                // do nothing
+            }
+        }
+        while (!in.isEmpty()) {
+            rq.enqueue(in.readString());
+        }
+        Iterator<String> iter = rq.iterator();
+        for (int i = 0; i < k && iter.hasNext(); i++) {
+            out.println(iter.next());
+        }
+    }
+}
