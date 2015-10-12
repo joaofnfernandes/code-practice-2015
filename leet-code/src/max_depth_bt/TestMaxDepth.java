28b8a4d5474b5d744c12a6350baea4394539f218
jff <joaofnfernandes@gmail.com>
2015-10-12 15:13:59 -0700

    finds max depth of a binary tree

---
+++ leet-code/src/max_depth_bt/TestMaxDepth.java
@@ -0,0 +1,28 @@
+package max_depth_bt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMaxDepth {
+
+    @Test
+    public void testEmptyTree(){
+        TreeNode tree = null;
+        assertEquals(0, new Solution().maxDepth(tree));
+    }
+    
+    @Test
+    public void testTreeSingleNode(){
+        TreeNode tree = new TreeNode(0);
+        assertEquals(1, new Solution().maxDepth(tree));
+    }
+    
+    @Test
+    public void testTree3LeftNods(){
+        TreeNode tree = new TreeNode(0);
+        tree.left = new TreeNode(1);
+        tree.left.left = new TreeNode(2);
+        assertEquals(3, new Solution().maxDepth(tree));
+    }
+}
---
+++ leet-code/src/max_depth_bt/TreeNode.java
@@ -0,0 +1,10 @@
+package max_depth_bt;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    
+    TreeNode(int x) { val = x; }
+}
