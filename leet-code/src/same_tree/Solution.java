06cb403ee58ed9d205081a310bebc24f58122a40
jff <joaofnfernandes@gmail.com>
2015-10-13 10:53:13 -0700

    checks if two trees are the same

---
+++ leet-code/src/same_tree/Solution.java
@@ -0,0 +1,21 @@
+package same_tree;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null) {
+            if(q == null) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if(q == null) {
+                return false;
+            } else {
+                return p.val == q.val &&
+                        isSameTree(p.left, q.left) &&
+                        isSameTree(p.right, q.right);
+            }
+        }
+    }
+}
---
+++ leet-code/src/same_tree/TreeNode.java
@@ -0,0 +1,9 @@
+package same_tree;
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
