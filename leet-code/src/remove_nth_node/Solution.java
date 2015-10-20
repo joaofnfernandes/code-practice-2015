722b20fbfefb55ef434da36a71226a21b2770b4d
jff <joaofnfernandes@gmail.com>
2015-10-20 12:09:14 -0700

    removes nth node from a linked list

---
+++ leet-code/src/remove_nth_node/Solution.java
@@ -0,0 +1,21 @@
+package remove_nth_node;
+
+// Removes nth node from the end of a linked list. We offset a pointer by n nodes, to find
+// the node to delete
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode ptr = head;
+        while(ptr != null && n-- > 0) {
+            ptr = ptr.next;
+        }
+        ListNode sentinel = new ListNode(-1);
+        sentinel.next = head;
+        head = sentinel;
+        while(ptr != null) {
+            ptr = ptr.next;
+            sentinel = sentinel.next;
+        }
+        sentinel.next = sentinel.next.next;
+        return head.next;
+    }
+}
