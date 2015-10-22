2283306c3c5e321761891f46ebead95aed9af414
jff <joaofnfernandes@gmail.com>
2015-10-22 15:47:02 -0700

    removes all elements with a given value from a list

---
+++ leet-code/src/remove_list_elements/ListNode.java
@@ -0,0 +1,10 @@
+package remove_list_elements;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ leet-code/src/remove_list_elements/Solution.java
@@ -0,0 +1,23 @@
+package remove_list_elements;
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(-1);
+        sentinel.next = head;
+        ListNode previous = sentinel;
+        while(head != null) {
+            if(head.val == val) {
+                previous.next = previous.next.next;
+            } else {
+                previous = previous.next;
+            }
+            
+            if(previous != null) {
+                head = previous.next;
+            } else {
+                head = null;
+            }
+        }
+        return sentinel.next;
+    }
+}
