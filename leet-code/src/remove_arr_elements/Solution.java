10da3ae9f7619265a094d8e2a69ab34a1e67d77c
jff <joaofnfernandes@gmail.com>
2015-11-01 18:31:04 -0800

    removes a value from an array of values

---
+++ leet-code/src/remove_arr_elements/Solution.java
@@ -0,0 +1,21 @@
+package remove_arr_elements;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, j = 0;
+        while(i < nums.length) {
+            if(nums[i] == val) {
+                i++;
+            } else {
+                swap(nums, i++, j++);
+            }
+        }
+        return j;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int aux = nums[i];
+        nums[i] = nums[j];
+        nums[j] = aux;
+    }
+}
