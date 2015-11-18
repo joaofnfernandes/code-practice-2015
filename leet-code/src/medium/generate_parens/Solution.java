abf12dcd8dec3cc00149c17ce461de484b44b833
jff <joaofnfernandes@gmail.com>
2015-11-18 14:19:41 -0800

    implemented a second solution for generating balanced parens

--- leet-code/src/medium/generate_parens/Solution.java
+++ leet-code/src/medium/generate_parens/Solution.java
@@ -7,11 +7,28 @@ import java.util.Set;
 
 public class Solution {
     public List<String> generateParenthesis(int n) {
-        Set<String> set = generateParenthesis(new HashSet<String>(), n, 0, "");
+        Set<String> set = generateParenthesis(new HashSet<String>(), n * 2, n, n, "");
         return new ArrayList<String>(set);
     }
     
-    public Set<String> generateParenthesis(Set<String> uniqueStrings, int total, int openParens, String stringSoFar) {
+    private Set<String> generateParenthesis(Set<String> uniqueStrings, int total, int left, int right, String stringSoFar) {
+        if(right < left) {
+            return uniqueStrings;
+        } else if(total == 0) {
+            uniqueStrings.add(stringSoFar);
+        } else {
+            generateParenthesis(uniqueStrings, total - 1, left - 1, right, stringSoFar + "(");
+            generateParenthesis(uniqueStrings, total - 1, left, right - 1, stringSoFar + ")");
+        }
+        return uniqueStrings;
+    }
+    
+    public List<String> generateParenthesis2(int n) {
+        Set<String> set = generateParenthesis2(new HashSet<String>(), n, 0, "");
+        return new ArrayList<String>(set);
+    }
+    
+    private Set<String> generateParenthesis2(Set<String> uniqueStrings, int total, int openParens, String stringSoFar) {
         if(total == 0) {
             while(openParens > 0) {
                 stringSoFar = stringSoFar + ")";
@@ -20,12 +37,12 @@ public class Solution {
             uniqueStrings.add(stringSoFar);
         } else {
             // open parens on left
-            generateParenthesis(uniqueStrings, total - 1, openParens + 1, "(" + stringSoFar);
+            generateParenthesis2(uniqueStrings, total - 1, openParens + 1, "(" + stringSoFar);
             // open parens of right
-            generateParenthesis(uniqueStrings, total - 1, openParens + 1,  stringSoFar + "(");
+            generateParenthesis2(uniqueStrings, total - 1, openParens + 1,  stringSoFar + "(");
             // close parens
             if(openParens > 0) {
-                generateParenthesis(uniqueStrings, total, openParens -1 ,  stringSoFar + ")");
+                generateParenthesis2(uniqueStrings, total, openParens -1 ,  stringSoFar + ")");
             }
         }
         return uniqueStrings;
