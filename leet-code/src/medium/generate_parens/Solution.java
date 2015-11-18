ad2e0108944f379863333b30d80b56e5875886b9
jff <joaofnfernandes@gmail.com>
2015-11-18 11:57:07 -0800

    implemented solution to generating balanced parens that passes unit tests

--- leet-code/src/medium/generate_parens/Solution.java
+++ leet-code/src/medium/generate_parens/Solution.java
@@ -7,22 +7,27 @@ import java.util.Set;
 
 public class Solution {
     public List<String> generateParenthesis(int n) {
-        Set<String> set = generateParenthesis(new HashSet<String>(), n, "");
+        Set<String> set = generateParenthesis(new HashSet<String>(), n, 0, "");
         return new ArrayList<String>(set);
     }
     
-    public Set<String> generateParenthesis(Set<String> set, int n, String stringSoFar) {
-        if(n < 1) {
-            return set;
-        } else if(n == 1) {
-            set.add("()" + stringSoFar);
-            set.add(stringSoFar + "()");
-            set.add("(" + stringSoFar + ")");
+    public Set<String> generateParenthesis(Set<String> uniqueStrings, int total, int openParens, String stringSoFar) {
+        if(total == 0) {
+            while(openParens > 0) {
+                stringSoFar = stringSoFar + ")";
+                openParens--;
+            }
+            uniqueStrings.add(stringSoFar);
         } else {
-            generateParenthesis(set, n - 1, "()" + stringSoFar);
-            generateParenthesis(set, n - 1, stringSoFar + "()");
-            generateParenthesis(set, n - 1, "(" + stringSoFar + ")");
+            // open parens on left
+            generateParenthesis(uniqueStrings, total - 1, openParens + 1, "(" + stringSoFar);
+            // open parens of right
+            generateParenthesis(uniqueStrings, total - 1, openParens + 1,  stringSoFar + "(");
+            // close parens
+            if(openParens > 0) {
+                generateParenthesis(uniqueStrings, total, openParens -1 ,  stringSoFar + ")");
+            }
         }
-        return set;
+        return uniqueStrings;
     }
 }
